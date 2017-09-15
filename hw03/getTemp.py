59bd81206fcfcd41440e78e12e4d232c9b90a9a7
Douglas Wise <wisedt@rose-hulman.edu>
2017-09-15 12:54:06 -0400

    fixed read data for tmp2

--- hw03/getTemp.py
+++ hw03/getTemp.py
@@ -11,7 +11,7 @@ address2 = 0x49
 
 while True:
     temp1 = bus.read_byte_data(address1, 0)
-    temp2 = bus.read_byte_data(address2)
+    temp2 = bus.read_byte_data(address2, 0)
     temp1 = temp1*1.8 + 32
     temp2 = temp2*1.8 + 32
     print(temp1, end="\r")
