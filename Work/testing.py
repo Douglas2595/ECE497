bffc93fcd32872cadd175efcc247c09a12240d75
Douglas Wise <wisedt@rose-hulman.edu>
2017-09-18 22:00:55 -0400

    added README and updating testing file

--- Work/testing.py
+++ Work/testing.py
@@ -5,13 +5,26 @@
 
 
 #map testing
-a = "a"
-b = "b"
+# a = "a"
+# b = "b"
+#
+# map1 = {a: 'A', b: 'B'}
+# map2 = {a: 'A2', b: 'B2'}
+#
+# print("Begin Test")
+# print("testing map1: " + map1[a] + " " + map1[b])
+# print("testing map2: " + map2[a] + " " + map2[b])
+# print("Test Finished")
 
-map1 = {a: 'A', b: 'B'}
-map2 = {a: 'A2', b: 'B2'}
+#or testing
+# a = 0x10
+# b = 0x01
+#
+# y = a | b
+# print('y = {}'.format(format(y, '02x')))
 
-print("Begin Test")
-print("testing map1: " + map1[a] + " " + map1[b])
-print("testing map2: " + map2[a] + " " + map2[b])
-print("Test Finished")
+#bit shift testing
+a = 0x01
+for i in range(1, 5):
+    print('a = {}'.format(format(a, '02x')))
+    a = a << 1
---
+++ hw03/README.md
@@ -0,0 +1,12 @@
+Douglas Wise
+Sep 18, 2017
+README for hw03
+
+Requirements for hw03:
+    1. tmp sensors      done
+    2. Etch-a-sketch    done
+
+Etch-a-sketch instructions:
+    1. Read README
+    2. Run Etchv3.py
+        host$ python3 Etchv3.py
